b8735d768d5877e4be6fd727d3b8164203726d32
vivek <vivek.bagde1203@gmail.com>
2025-11-20 02:09:36 +0530

    adding stage

--- vars/mavenBuild.groovy
+++ vars/mavenBuild.groovy
@@ -1,4 +1,5 @@
-def call(map config = [:]) {
+import com.vbagde.pipeline.MavenHelper
+def call(Map config = [:]) {
     def mvn = new MavenHelper(steps)
     mvn.setup()
     mvn.build(config.goal ?: "clean package")
